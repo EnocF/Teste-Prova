d5d63d5ecf938894d6ab1993b0102710b4c0412d
EnocF <equipedba@gmail.com>
2021-08-21 15:03:58 -0400

    Add files via upload

--- Controle/con_editar.php
+++ Controle/con_editar.php
@@ -1,30 +1,14 @@
 <?php
 
-	if(isset($_POST['Update'])) {
-		$id = $_POST['Id'];
-		$model = $_POST['model'];
+$id = $_POST['Id'];
+$model = $_POST['model'];
         
-        //Conexão db
-		include_once("../SQL/con.php");
+//Conexão db
+include_once("../SQL/con.php");
 
-	// update user data
+// update nodel
 	$result = mysqli_query($mysqli, "UPDATE DQCMODEL SET id='$id',Model='$model' WHERE id=$id");
 
-	// Redirect to homepage to display updated user in list
+// Retornar para Index
 	header("Location: ../index.php");
-}
-?>
-<?php
-// Display selected user data based on id
-// Getting id from url
-$id = $_GET['id'];
-
-// Fetech user data based on id
-$result = mysqli_query($mysqli, "SELECT * FROM DQCMODEL WHERE id=$id");
-
-while($user_data = mysqli_fetch_array($result))
-{
-	$id = $user_data['Id'];
-	$model = $user_data['model'];
-}
 ?>
