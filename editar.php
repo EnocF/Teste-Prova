ed6f231f701d5eeba496c7f9b8088ba37fcbad0a
EnocF <equipedba@gmail.com>
2021-08-21 14:58:57 -0400

    Add files via upload

--- editar.php
+++ editar.php
@@ -1,18 +1,30 @@
+<?php 
+include_once("SQL/con.php");
+	
+//Mostrar conteúdo baseado no id
+$id = $_GET['id'];
+
+$result = mysqli_query($mysqli, "SELECT * FROM DQCMODEL WHERE Id='$id'");
+$e_model = mysqli_fetch_array($result);
+	$id = $e_model['Id'];
+	$model = $e_model['Model'];
+
+?>
 <html>
-    
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1"/>
-	<title>Edit User Data</title>
+	<title>Editar Modelo</title>
 </head>
 
 <body>
-	<a href="index.php">Home</a>
+	<a href="index.php">Retornar</a>
 	<br/><br/>
 
 	<form name="edit" method="post" action="Controle/con_editar.php">
 		<table border="0">
 			<tr>
-				<td>Id</td>
+				<td>Id  <?php echo $id;?></title>
+</head></td>
 				<td><input type="text" name="Id" value=<?php echo $id;?>></td>
 			</tr>
 			<tr>
@@ -20,7 +32,7 @@
 				<td><input type="text" name="model" value=<?php echo $model;?>></td>
 			</tr>
 			<tr>
-				<td><input type="hidden" name="id" value=<?php echo $_GET['Id'];?>></td>
+				<td><input type="hidden" name="id" value=<?php echo $_GET['id'];?>></td>
 				<td><input type="submit" name="Update" value="Update"></td>
 			</tr>
 		</table>
