5581e901c67871b538f7a39fedecd5577c27dfd8
EnocF <equipedba@gmail.com>
2021-08-21 17:03:12 -0400

    Update imprimir2.php

--- Controle/imprimir2.php
+++ Controle/imprimir2.php
@@ -4,13 +4,13 @@ include_once("../SQL/con.php");
 
 $result = mysqli_query($mysqli, "SELECT * FROM DQCMODEL");
 header("Content-type: application/vnd.ms-excel");
- header("Content-type: application/force-download");
- header("Content-Disposition: attachment; filename=teste.xls");
- header("Pragma: no-cache");
+header("Content-type: application/force-download");
+header("Content-Disposition: attachment; filename=teste.xls");
+header("Pragma: no-cache");
 ?>
 
 <meta charset='utf-8'>
-    <table width='80%' border=1>
+ <table width='80%' border=1>
         <tr>TABELA: DQCMODEL</tr>
     <tr>
         <th>ID</th> <th>Model</th> <th>Operações</th>
@@ -20,7 +20,7 @@ header("Content-type: application/vnd.ms-excel");
         echo "<tr>";
         echo "<td>".$model['Id']."</td>";
         echo "<td>".$model['Model']."</td>";
-        echo "<td><a href='editar.php?id=$model[Id]'>Edit</a> | <a href='Controle/con_delete.php?Id=$model[Id]'>Delete</a></td></tr>";
+        echo "</tr>";
     }
     ?>
-    </table>
+ </table>
