b30b48f8698a82ecf3382fbcdce48d1724fb3ff5
EnocF <equipedba@gmail.com>
2021-08-21 10:30:10 -0400

    Add files via upload

--- inserir.php
+++ inserir.php
@@ -12,15 +12,15 @@
 		<table width="25%" border="0">
 			<tr>
 				<td>Id</td>
-				<td><input type="text" name="Id"></input></td>
+				<td><input type="text" name="Id"/></td>
 			</tr>
 			<tr>
 				<td>Model</td>
-				<td><input type="text" name="Model"></td>
+				<td><input type="text" name="Model"/></td>
 			</tr>
 			<tr>
 				<td></td>
-				<td><input type="submit" name="Submit" value="Add"></td>
+				<td><input type="submit" name="Submit" value="Add"/></td>
 			</tr>
 		</table>
 	</form>
